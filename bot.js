9c6fcb0ef7ed52a1e405eb61b0893eff8f2b43c6
Mike Phe <mikephee00@gmail.com>
2019-02-23 23:46:11 -0800

    Ping app to keep alive

--- bot.js
+++ bot.js
@@ -1,9 +1,16 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('expr;ess')
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const https = require("https");
+const secToPing = 300000;
 const discord = require("discord.js");
 const bot = new discord.Client();
 const token = "secret-key";
+
+// Pings app to keep alive
+setInterval(function() {
+    https.get("http://<your app name>.herokuapp.com");
+}, secToPing); // every 5 minutes (300000)
 
 bot.on("ready", () => {
   console.log("BEEP BOOP!");
